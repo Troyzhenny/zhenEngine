38d42b229fc04a3b1d6bbdfdd3c4d7c2c352086f
Troyzhen <tropicboytroy@gmail.com>
2025-12-31 05:30:15 -0500

    init glfw and glad - in progess

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,35 @@
+#define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "terminal_colors.h"
 
 
 int main()
 {
-    return 0;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // create window object
+    GLFWwindow* window = glfwCreateWindow(500, 500, "Active Window", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout<< "\033[1;31mFailed to create GLFW window\033[0m" <<std::endl;
+        glfwTerminate();
+
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << LIGHT_RED << "Failed to initialize GLAD" << RESET << std::endl;
+        return -1;
+    }
+
 }
